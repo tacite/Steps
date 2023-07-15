a9b3482cf6c30af7fa5298d7a60dd71750332869
David Tacite <david.tacite@gmail.com>
2023-07-15 15:33:49 +0200

    Initial Commit

---
+++ Steps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Steps
+//
+//  Created by David Tacite on 15/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Steps/StepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsApp.swift
+//  Steps
+//
+//  Created by David Tacite on 15/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct StepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
